b7ca5d35edced38d8a1db3f820b92fbead886476
Y, Ryota <yakushi@isonly.net>
2021-12-31 22:42:49 +0900

    A Starting Point

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::{
+    io::stdin,
+    process::Command,
+};
+
+fn main() {
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+
+    // read_line leaves a trailing newline, which trim removes
+    let command = input.trim();
+
+    Command::new(command)
+        .spawn()
+        .unwrap();
+}
